fad47ac48f8758d9f9334c9d6dfb1a2160d0f6fe
sogand <sogandprh@gmail.com>
2022-08-21 16:33:38 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//testData1
+let MarksMass=78;
+let Marksheight=1.69;
+let JohnMass=92;
+let Johansheight=1.95;
+let JohansBMI =JohnMass / Johansheight**2;
+let MarksBMI =MarksMass / Marksheight**2;
+let MarkHigherBMI=console.log(MarksBMI>JohansBMI);
+//////////////////////////////////////////////
+//testData1=
+ MarksMass=95;
+ Marksheight=1.88;
+ JohnMass=85;
+ Johansheight=1.76;
+JohansBMI =JohnMass / Johansheight**2;
+MarksBMI =MarksMass / Marksheight**2;
+MarkHigherBMI=console.log(MarksBMI>JohansBMI);
